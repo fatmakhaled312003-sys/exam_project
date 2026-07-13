7f0327400e7573c77fe505f7ccc3cbf826935ae6
fatmakhaled312003-sys <fatmakhaled312003@gmail.com>
2026-07-13 12:57:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -234,16 +234,21 @@ if generate_button:
                 break
 
     write_rtl(...)
+  q_index = 1
+
+for i in range(q_start_count):
+    while True:
+        sura = random.choice(selected_suwar)
+        maqta = random.choice(suwar_database[sura]["start"])
+
+        key = (sura, maqta["from"], maqta["to"])
+
+        if key not in used_questions:
+            used_questions.add(key)
+            break
+
+    write_rtl(...)
     q_index += 1
-for j in range(q_middle_count):
-        while True:
-            sura = random.choice(selected_suwar)
-            macta = random.choice(suwar_database[sura]["middle"])
-    
-            key = (sura, macta["from"], macta["to"])
-            if key not in used_questions:
-                used_questions.add(key)
-                break
             write_rtl(f"<b>س {q_index}:</b> اقرأ مستعيناً بالله تعالى من <b>سورة {sura}</b> من قوله تعالى: [ {macta['from']} ] إلى قوله تعالى: [ {macta['to']} ].", size="17px")
             q_index += 1
 
